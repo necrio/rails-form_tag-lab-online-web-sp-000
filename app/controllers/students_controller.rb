2ebf8f76d3304d117bdba5aafb495c39a81c337a
Keith Isham <kisham8899@gmail.com>
2019-12-06 12:20:27 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,8 @@ class StudentsController < ApplicationController
   end 
   
   def create 
-    
+    Student.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
   end 
 
 end
