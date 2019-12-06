0b47b25dd2d00ffbea7a4eff9fdb5dc912960181
Keith Isham <kisham8899@gmail.com>
2019-12-06 12:24:42 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,5 +17,11 @@ class StudentsController < ApplicationController
    session[:form_params] = params.inspect
    redirect_to new_student_path
   end 
+  
+  private 
+  
+  def set_student
+    @student = Student.find(params[:id])
+  end 
 
 end
